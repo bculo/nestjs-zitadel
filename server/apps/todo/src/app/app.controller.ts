cda4be63e610ec985438aabb4545309c043761bd
bculo <culobozo@gmail.com>
2024-05-08 11:00:42 +0200

    feat: adds swagger

--- server/apps/todo/src/app/app.controller.ts
+++ server/apps/todo/src/app/app.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
-@Controller()
+@Controller("api")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("test")
   getData() {
     return this.appService.getData();
   }
--- server/apps/todo/src/main.ts
+++ server/apps/todo/src/main.ts
@@ -4,18 +4,27 @@
  */
 
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo example')
+    .setDescription('The Todo API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}`
   );
 }
 
