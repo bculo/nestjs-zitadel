0c68f51c9d8adc9bbb1b7fe84cf97e439704947a
bculo <culobozo@gmail.com>
2024-05-18 14:36:48 +0200

    feat: cleanup

--- server/apps/todo/src/app/common/middlewares/logger.middleware.ts
+++ server/apps/todo/src/app/common/middlewares/logger.middleware.ts
@@ -8,7 +8,6 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request...');
     next();
   }
 }
